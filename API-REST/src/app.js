d973e2afeb34c50ba2a932e5cae58f70dd52de9c
dvaaz <darley.azevedo@gmail.com>
2026-02-10 21:28:16 -0300

    commit final da aula

--- API-REST/src/app.js
+++ API-REST/src/app.js
@@ -5,6 +5,17 @@ const app = express()
 // Indicar para o express ler o body como json
 app.use(express.json())
 
+// funcoes
+function findSelecaoById(id) { // busca o Objeto inteiro
+    let index = parseInt(id) // força a transformação da string em number evitando erros
+    return selecoes.find(selecao => selecao.id === index)
+}
+
+function findIndexSelecao(id) {
+    let index = parseInt(id) // força a transformação da string em number evitando erros
+    return selecoes.findIndex(selecao => selecao.id === index)
+}
+
 //Mock
 const selecoes = [
     {id:1, selecao: 'Brasil', grupo:'C'},
@@ -24,22 +35,40 @@ app.get('/selecoes',(req,res)=>{
     res.status(200).send(selecoes)
 })
 
-app.get('/selecoes/:id',(request, response)=>{
-    const id = parseInt(request.params.id)
-    const selecao = selecoes.find(s => s.id === id)
+app.get('/selecoes/:id',(req, res)=>{
+
+    console.log(req.params.id)
+    let selecao = findSelecaoById(req.params.id)
+
     
     if (selecao) {
-    response.status(200)
-    .send(selecoes)
+    res.status(200)
+    .send(selecao)
     } else {
-        
+    res.status(404)
+    .send('Seleção não encontrada')
     }
 })
 
-// POST, selecoes
+// POST, selecao
 app.post('/selecoes', (req,res)=> {
     selecoes.push(req.body)
     res.status(200).send('Seleção cadastrada com sucesso!')
+})
+
+// DELETE, selecao
+app.delete('/selecoes/:id', (req,res)=>{
+    console.log(req.params.id)
+    let selecao = findIndexSelecao(req.params.id)
+
+    if (selecao) {
+        selecoes.splice(selecao, 1)
+        res.status(200)
+        .send(selecoes)
+    } else {
+        res.status(404)
+        .send('Seleção não encontrada')
+    }
 })
 
 
